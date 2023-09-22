b504984bba0ff51c91b93afa8c610da258c21e1e
Fabien Streit <streit.fabien@gmail.com>
2023-09-23 00:52:52 +0200

    Godot 4

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A small editor widget which provides basic functions for tracking your time with
 
 ![Plugin preview](/images/preview-1.png)
 
-Developed in and tested against Godot 3.2.
+Developed in and tested against Godot 4.1.1.
 
 ## Installation
 Clone this repository or download it as ZIP. You only need to put `addons/time-tracker` folder inside your project folder. ZIP already has only necessary files.
--- addons/time-tracker/plugin.gd
+++ addons/time-tracker/plugin.gd
@@ -1,4 +1,4 @@
-tool
+@tool
 extends EditorPlugin
 
 var _dock_instance : Control
@@ -6,22 +6,22 @@ var _dock_instance : Control
 const STORED_SESSIONS_PATH : String = "res://.tracked-sessions.json"
 
 func _enter_tree() -> void:
-	_dock_instance = load("res://addons/time-tracker/TrackerDock.tscn").instance()
+	_dock_instance = preload("res://addons/time-tracker/TrackerDock.tscn").instantiate()
 	_dock_instance.name = "TimeTracker"
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_BL, _dock_instance)
 	
 	_load_sessions()
-	_dock_instance.connect("sessions_changed", self, "_store_sessions")
-	_dock_instance.connect("save_requested", self, "_store_sessions_to_file")
-	_dock_instance.connect("restore_requested", self, "_load_sessions_from_file")
+	_dock_instance.sessions_changed.connect(_store_sessions)
+	_dock_instance.save_requested.connect(_store_sessions_to_file)
+	_dock_instance.restore_requested.connect(_load_sessions_from_file)
 	
 	# Try to find controls immediately (it will fail if not ready yet).
 	_on_editor_base_ready()
 	# Also connect to the ready signal, so that it is correctly detected then.
 	var editor_base = get_editor_interface().get_base_control()
-	editor_base.connect("ready", self, "_on_editor_base_ready")
+	editor_base.ready.connect(_on_editor_base_ready)
 	# And connect to the signal that will trigger when a user actually interacts with top buttons.
-	connect("main_screen_changed", self, "_on_main_screen_changed")
+	main_screen_changed.connect(_on_main_screen_changed)
 
 func _exit_tree() -> void:
 	_store_sessions()
@@ -33,21 +33,23 @@ func _load_sessions() -> void:
 	_load_sessions_from_file(STORED_SESSIONS_PATH)
 
 func _load_sessions_from_file(file_path : String) -> void:
-	var file = File.new()
-	if (!file.file_exists(file_path)):
+	if (!FileAccess.file_exists(file_path)):
+		print("No file " + file_path + " found")
 		return
 	
-	var error = file.open(file_path, File.READ)
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	var error = FileAccess.get_open_error()
 	if (error != OK):
 		printerr("Failed to open sessions file '" + file_path + "' for reading: Error code " + str(error))
 		return
 	
-	var stored_string = file.get_as_text()
-	var parse_result = JSON.parse(stored_string)
-	if (parse_result.error != OK):
-		printerr("Failed to parse tracked sessions: Error code " + str(parse_result.error))
+	var json = JSON.new()
+	var parse_result = json.parse_string(file.get_as_text())
+	var parse_error = json.get_error_message()
+	if (parse_error != ""):
+		printerr("Failed to parse tracked sessions: Error code " + parse_error)
 	else:
-		var stored_sessions = parse_result.result
+		var stored_sessions = parse_result
 		if (stored_sessions is Array && stored_sessions.size() > 0):
 			_dock_instance.restore_tracked_sessions(stored_sessions)
 	
@@ -58,10 +60,10 @@ func _store_sessions() -> void:
 
 func _store_sessions_to_file(file_path : String) -> void:
 	var tracked_sessions = _dock_instance.get_tracked_sessions()
-	var stored_string = JSON.print(tracked_sessions, "  ")
+	var stored_string = JSON.stringify(tracked_sessions, "  ")
 	
-	var file = File.new()
-	var error = file.open(file_path, File.WRITE)
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	var error = FileAccess.get_open_error()
 	if (error != OK):
 		printerr("Failed to open sessions file '" + file_path + "' for writing: Error code " + str(error))
 		return
@@ -111,7 +113,7 @@ func _on_editor_base_ready() -> void:
 	var main_screen_buttons = editor_main_button_vb.get_children()
 	
 	for button_node in main_screen_buttons:
-		if !(button_node is ToolButton):
+		if !(button_node is Button):
 			continue
 		if (button_node.pressed):
 			_on_main_screen_changed(button_node.text)
